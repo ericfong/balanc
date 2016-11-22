e82385dc10de43dcafae26774f279c936bdb4d64
Eric <eric.fong@schooltracs.com>
2016-11-22 18:19:20 +0800

    presistent patchStore

--- src/patchStore.js
+++ src/patchStore.js
@@ -1,20 +1,14 @@
 import Collection from 'nedb-promise'
 
 
-function promiseToCallback(args) {
-  return function(err, ret) {
-    if (err) return args[1](err)
-    args[0](ret)
-  }
-}
-
-
 export default function PatchCollection(options) {
   const coll = new Collection(options)
   const nedbInstance = coll.nedb
 
-  coll.operations = []
+  coll.operations = new Collection()
   coll.operationNumber = 0
+
+
 
   // hack via nedb-promise
   Object.assign(coll, {
@@ -25,42 +19,43 @@ export default function PatchCollection(options) {
     },
 
     _insertOperation(fn, args) {
-      coll.operations.push({_id: coll.operationNumber++, fn, args})
-      // persist operations
-
-      // use promise to debounce
-      if (this._promise) return
-      this._promise = new Promise(resolve => setTimeout(resolve, 1))
-      .then(() => this.hook(coll.operations))
-      .then(newOps => {
-        coll.operations = newOps || []
-
-        this._promise = null
+      return coll.operations.insert({_id: coll.operationNumber++, fn, args})
+      .then(() => {
+        // use promise to debounce
+        if (this._promise) return
+        this._promise = coll.operations.find({})
+        .then(ops => this.hook(ops))
+        .then(doneOpIds => {
+          if (doneOpIds) {
+            return this.__update({
+              _id: {$in: doneOpIds},
+            }, {
+              $set: {
+                doneAt: new Date(),
+              },
+            })
+          }
+        })
+        .then(() => this._promise = null)
       })
     },
 
+    __insert: coll.insert,
     insert(newDoc) {
-      this._insertOperation('insert', [newDoc])
-
-      return new Promise(function() {
-        nedbInstance.insert(newDoc, promiseToCallback(arguments))
-      })
+      return this._insertOperation('insert', [newDoc])
+      .then(() => this.__insert(newDoc))
     },
 
-    async update(query, updateQuery, options) {
-      this._insertOperation('update', [query, updateQuery, options])
-
-      return new Promise(function() {
-        nedbInstance.update(query, updateQuery, options, promiseToCallback(arguments))
-      })
+    __update: coll.update,
+    update(query, updateQuery, options) {
+      return this._insertOperation('update', [query, updateQuery, options])
+      .then(() => this.__update(query, updateQuery, options))
     },
 
-    async remove(query, options) {
-      this._insertOperation('remove', [query, options])
-
-      return new Promise(function() {
-        nedbInstance.remove(query, options, promiseToCallback(arguments))
-      })
+    __remove: coll.remove,
+    remove(query, options) {
+      return this._insertOperation('remove', [query, options])
+      .then(() => this.__remove(query, options))
     },
   })
 
--- test/patchStore.test.js
+++ test/patchStore.test.js
@@ -18,19 +18,16 @@ describe('patchStore', function() {
       should(ops[0].args[0]).properties({hello: 'world'})
       should(ops[0]._id).equal(0)
 
-
       // api call
       const doneOpIds = [0]
-
-      // filter logic still in setHook?
-      return _.filter(ops, op => !_.includes(doneOpIds, op._id))
+      return doneOpIds
     })
 
     await coll.insert({hello: 'world'})
     await coll.find({})
+    should(await coll.operations.count({})).equal(1)
 
     // hook is called
-    should(coll.operations).length(0)
     should(JSON.parse(lastOpsJson)).length(1)
   })
 })
